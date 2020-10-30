97b09ceb79c374a080732e33e0a952c30f3592cf
Ola Ouso <ouso.ola@gmail.com>
2020-10-30 10:09:20 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -27,7 +27,7 @@ handleSubmit = event => {
 
   render() {
     return (
-      <form>
+      <form onSubmit={event=> this.handleSubmit(event)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
